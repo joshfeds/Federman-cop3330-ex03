709088199a2c175b4197e06a3adfa14f72d3a2b5
joshfeds14 <joshfeds14@gmail,com>
2021-06-06 15:42:10 -0400

    Exercise 03

--- src/main/java/oop/example/Exercise03.java
+++ src/main/java/oop/example/Exercise03.java
@@ -9,10 +9,10 @@ public class Exercise03 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
-        System.out.print("What is the quote?");
+        System.out.print("What is the quote? ");
         String quote = input.nextLine();
 
-        System.out.print("Who said it?");
+        System.out.print("Who said it? ");
         String person = input.nextLine();
 
         System.out.printf(person + " says," + "\"%s\"", quote);
